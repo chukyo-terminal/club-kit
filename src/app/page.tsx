0fb61e745a03e55dde4d7c3ce2dfe4589d1d0017
LimeIvy <raimu10311221@yahoo.co.jp>
2026-02-01 04:32:13 +0900

    nextjsプロジェクトの初期化

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center rounded-lg bg-blue-500 p-4 text-white">
+      <h1 className="font-bold text-2xl">Club Kit</h1>
+    </div>
+  );
+}
